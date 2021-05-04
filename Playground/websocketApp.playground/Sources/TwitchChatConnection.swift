53b923fbb96cf21b163463d198bdeb82fb51b678
d1mka112 <d1mka@users.noreply.github.com>
2021-05-05 00:31:26 +0300

    Added protocol TwitchChatConnectionDelegate

--- Playground/websocketApp.playground/Sources/TwitchChatConnection.swift
+++ Playground/websocketApp.playground/Sources/TwitchChatConnection.swift
@@ -5,7 +5,12 @@ import Foundation
  - add onJoinChannel()
 */
 
+protocol TwitchChatConnectionDelegate {
+    func onChatMessage()
+    func onJoinChannel()
+}
 
+//this structure will set all the values of its own properties from the raw data itself
 struct TwitchChatMessage {
     var nickname: String
     var message: String
@@ -33,9 +38,11 @@ class TwitchChatConnection {
     
     var channel: String = ""
     
+    var delegate: TwitchChatConnectionDelegate?
+    
     var webSocketTask = URLSession(configuration: .default).webSocketTask(with: URL(string: "wss://irc-ws.chat.twitch.tv:443")!)
     
-    func sendMessage(message: String) {
+    func sendMessage(_ message: String) {
         let urlMessage = URLSessionWebSocketTask.Message.string(message)
         webSocketTask.send(urlMessage) { error in
             if error != nil {
@@ -46,19 +53,26 @@ class TwitchChatConnection {
     
     func connectToTheServer() {
         webSocketTask.resume()
-        self.sendMessage(message: "PASS \(token)")
-        self.sendMessage(message: "NICK \(nickname.lowercased())")
+        self.sendMessage("PASS \(token)")
+        self.sendMessage("NICK \(nickname.lowercased())")
     }
     func connectToTheChatChannel(into channel: String) {
-        self.sendMessage(message: "JOIN #\(channel)")
+        self.sendMessage("JOIN #\(channel)")
     }
     
-    func sendMessageToTheChat(message: String) {
+    func sendMessageToTheChat(_ message: String) {
         if channel != "" {
-            self.sendMessage(message: "PRIVMSG #\(channel) :\(message)")
+            self.sendMessage("PRIVMSG #\(channel) :\(message)")
         }
         else {
             print("You are not join to the channel!")
+        }
+    }
+    
+    func onServerMessage(_ message: String) {
+        if(message.contains(":\(nickname).tmi.twitch.tv 366 \(nickname) #\(channel) :End of /NAMES list")) {
+            delegate?.onJoinChannel()
+            //is that normal?
         }
     }
     
@@ -68,8 +82,8 @@ class TwitchChatConnection {
                 case .success(let urlMessage):
                     switch urlMessage {
                         case .string(let message):
-                            print(message)
-                            let chatMessage = TwitchChatMessage(rawMessage: message)
+                            //print(message)
+                            self.onServerMessage(message)
                             //self.delegate?.onMessage(message: message)
                         default:
                             print("Error of reading message")
